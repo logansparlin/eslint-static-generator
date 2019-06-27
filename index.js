04c8c8d02f3b0c2738cee6ba51a7ebfa88577de4
Logan Sparlin <logan.sparlin@gmail.com>
2019-06-27 16:59:07 -0500

    Command line arguments

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const clui = require('clui');
 const lintRules = require('./.eslintrc.js').rules;
 const Mustache = require('mustache');
 const chalk = require('chalk');
+const program = require('commander');
 
 const ESLINT_BASE_PATH = 'https://eslint.org/docs/rules'
 const OUTPUT_PATH = './dist'
 
+program.version('1.0.0')
+  .option('-h, --html', 'Export HTML File')
+  .option('-j, --json', 'Export JSON File')
+  .parse(process.argv);
+
 class SiteGenerator {
   constructor() {
     this.rules = Object.keys(lintRules);
-    this.spinner = new clui.Spinner("Writing file...");
+    this.spinner = new clui.Spinner('Writing file...');
 
     if(!this.rules || this.rules.length <= 0) {
       throw new Error('You must provide a properly formatted .eslintrc file')
@@ -31,11 +37,11 @@ class SiteGenerator {
     return rules;
   }
 
-  startSpinner() {
+  startSpinner(type) {
     this.spinner.start()
   }
 
-  stopSpinner() {
+  stopSpinner(type) {
     this.spinner.stop()
   }
 
@@ -45,24 +51,46 @@ class SiteGenerator {
     }
   }
 
-  async writeFile(filename) {
+  async generateHTMLFile() {
+    this.startSpinner();
     const template = fs.readFileSync('./template.mustache', 'utf8')
     const rules = await this.getRuleList()
     const html = await Mustache.render(template, { rules })
 
-    this.startSpinner();
     this.createOutputDirectory()
-    fs.writeFile(`${OUTPUT_PATH}/${filename}`, html, err => {
+    
+    fs.writeFile(`${OUTPUT_PATH}/index.html`, html, 'utf8', err => {
       if(err) {
         return console.log(chalk.red("Couldn't write file."));
       }
 
       this.stopSpinner();
-      console.log(chalk.green(`The html was successfully generated and saved at ${OUTPUT_PATH}/${filename}`));
+      console.log(chalk.green(`The html was successfully generated and saved at ${OUTPUT_PATH}/index.html`));
+    });
+  }
+
+  async generateJSONFile() {
+    this.startSpinner();
+    const rules = await this.getRuleList()
+
+    this.createOutputDirectory()
+    fs.writeFile(`${OUTPUT_PATH}/rules.json`, JSON.stringify({ rules }), 'utf8', err => {
+      if(err) {
+        return console.log(chalk.red("Couldn't write file."));
+      }
+
+      this.stopSpinner();
+      console.log(chalk.green(`The JSON was successfully generated and saved at ${OUTPUT_PATH}/rules.json`));
     })
   }
 }
 
 const staticRuleSite = new SiteGenerator();
 
-staticRuleSite.writeFile('index.html');
+if(program.html) {
+  staticRuleSite.generateHTMLFile();
+}
+
+if(program.json) {
+  staticRuleSite.generateJSONFile();
+}
