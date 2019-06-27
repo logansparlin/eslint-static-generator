2915e0972a8eae7f305d6f50d1c1c67df71f144f
Logan Sparlin <logan.sparlin@gmail.com>
2019-06-27 16:26:16 -0500

    use template and improve terminal output

--- index.js
+++ index.js
@@ -1,77 +1,64 @@
 const fs = require('fs');
 const requestPromise = require('request-promise');
 const $ = require('cheerio');
+const clui = require('clui');
 const lintRules = require('./.eslintrc.js').rules;
+const Mustache = require('mustache');
+const chalk = require('chalk');
 
 const ESLINT_BASE_PATH = 'https://eslint.org/docs/rules'
-const PUBLIC_PATH = '.'
-
-const openingHtml = function() {
-  return (
-    `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <style>
-          .rule-table {
-            max-width: 800px;
-            margin: 0 auto;
-          }
-        </style>
-      </head>
-      <body>
-        <table class="rule-table">
-          <tr>
-            <th>Rule</th>
-            <th>Short Description</th>
-          </tr>\n`
-  )
-}
-
-const closingHtml = function() {
-  return (
-    `\n</table>\n</body>\n</html>`
-  )
-}
+const OUTPUT_PATH = './dist'
 
 class SiteGenerator {
   constructor() {
-    this.rules = Object.keys(lintRules)
+    this.rules = Object.keys(lintRules);
+    this.spinner = new clui.Spinner("Writing file...");
 
     if(!this.rules || this.rules.length <= 0) {
       throw new Error('You must provide a properly formatted .eslintrc file')
     }
   }
 
-  async generateHTML() {
-    let nodes = []
+  async getRuleList() {
+    let rules = []
     for( const rule of this.rules) {
       const html = await requestPromise(`${ESLINT_BASE_PATH}/${rule}`)
       const description = $('h1', html).text()
-      nodes.push(
-        `<tr class="rule-row">
-          <td><a href="${ESLINT_BASE_PATH}/${rule}" target="_blank">${rule}</a></td>
-          <td class="description">${description}</td>
-        </tr>`
-      )
+      const link = `${ESLINT_BASE_PATH}/${rule}`
+      rules.push({ rule, link, description })
     }
 
-    return nodes.join('\n')
+    return rules
+  }
+
+  startSpinner() {
+    this.spinner.start()
+  }
+
+  stopSpinner() {
+    this.spinner.stop()
+  }
+
+  createOutputDirectory() {
+    if(!fs.existsSync(OUTPUT_PATH)) {
+      fs.mkdirSync(OUTPUT_PATH)
+    }
   }
 
   async writeFile(filename) {
-    console.log('Writing file...')
-    const bodyHTML = await this.generateHTML()
-    const html = await `${openingHtml()} ${bodyHTML} ${closingHtml()}`
-    fs.writeFile(`${PUBLIC_PATH}/${filename}`, html, err => {
+    this.startSpinner()
+    const template = fs.readFileSync('./template.mustache', 'utf8')
+    const rules = await this.getRuleList()
+    const html = await Mustache.render(template, { rules })
+    
+    this.createOutputDirectory()
+    fs.writeFile(`${OUTPUT_PATH}/${filename}`, html, err => {
       if(err) {
         return console.error("Couldn't write file.")
       }
 
-      console.log(`The html was successfully generated and saved at ${PUBLIC_PATH}/${filename}`)
+      this.stopSpinner()
+      console.log(chalk.green(`The html was successfully generated and saved at ${OUTPUT_PATH}/${filename}`))
     })
   }
 }
