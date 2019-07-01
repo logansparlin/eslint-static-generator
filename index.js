ff3c09c63fbd9ba566c4be5ff7b842d99a1393a1
Logan Sparlin <logan.sparlin@gmail.com>
2019-07-01 11:25:15 -0500

    More command line options

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const requestPromise = require('request-promise');
 const $ = require('cheerio');
 const clui = require('clui');
-const lintRules = require('./.eslintrc.js').rules;
 const Mustache = require('mustache');
 const chalk = require('chalk');
 const program = require('commander');
 
 const ESLINT_BASE_PATH = 'https://eslint.org/docs/rules'
-const OUTPUT_PATH = './dist'
+const HTML_FILENAME = 'eslint-rules.html'
+const JSON_FILENAME = 'eslint-rules.json'
 
 program.version('1.0.0')
   .option('-h, --html', 'Export HTML File')
   .option('-j, --json', 'Export JSON File')
+  .option('-i, --input <input>', 'Path to the eslint rule file')
+  .option('-o, --output <output>', 'Path to output files to')
   .parse(process.argv);
+
+const OUTPUT_PATH = program.output || './dist'
+
+if(!program.input) {
+  return console.error(chalk.red('You must provide an input (--input) .eslintrc file'));
+}
 
 class SiteGenerator {
   constructor() {
-    this.rules = Object.keys(lintRules);
-    this.spinner = new clui.Spinner('Writing file...');
+    let ruleFile = require(program.input);
+    this.rules = ruleFile && Object.keys(ruleFile.rules);
+    this.spinner = new clui.Spinner();
 
     if(!this.rules || this.rules.length <= 0) {
-      throw new Error('You must provide a properly formatted .eslintrc file')
+      return console.error(chalk.red('You must provide a properly formatted .eslintrc file'));
     }
   }
 
-  async getRuleList() {
+  async getRulesList() {
     let rules = []
     for( const rule of this.rules) {
       const html = await requestPromise(`${ESLINT_BASE_PATH}/${rule}`)
@@ -37,11 +47,17 @@ class SiteGenerator {
     return rules;
   }
 
-  startSpinner(type) {
+  startSpinner() {
+    if(program.html) {
+      this.spinner.message(`Writing ${HTML_FILENAME} and ${JSON_FILENAME} file to ${OUTPUT_PATH}`);
+    } else if(!program.html) {
+      this.spinner.message(`Writing ${JSON_FILENAME} to ${OUTPUT_PATH}`);
+    }
+
     this.spinner.start()
   }
 
-  stopSpinner(type) {
+  stopSpinner() {
     this.spinner.stop()
   }
 
@@ -51,46 +67,30 @@ class SiteGenerator {
     }
   }
 
-  async generateHTMLFile() {
+  async generateStaticFiles() {
     this.startSpinner();
-    const template = fs.readFileSync('./template.mustache', 'utf8')
-    const rules = await this.getRuleList()
-    const html = await Mustache.render(template, { rules })
+    const rules = await this.getRulesList();
 
     this.createOutputDirectory()
-    
-    fs.writeFile(`${OUTPUT_PATH}/index.html`, html, 'utf8', err => {
-      if(err) {
-        return console.log(chalk.red("Couldn't write file."));
-      }
 
-      this.stopSpinner();
-      console.log(chalk.green(`The html was successfully generated and saved at ${OUTPUT_PATH}/index.html`));
-    });
-  }
+    if(program.html) {
+      const template = fs.readFileSync('./template.mustache', 'utf8')
+      const html = await Mustache.render(template, { rules })
+      fs.writeFileSync(`${OUTPUT_PATH}/${HTML_FILENAME}`, html, 'utf8')
+    }
 
-  async generateJSONFile() {
-    this.startSpinner();
-    const rules = await this.getRuleList()
+    fs.writeFileSync(`${OUTPUT_PATH}/${JSON_FILENAME}`, JSON.stringify({ rules }), 'utf8')
 
-    this.createOutputDirectory()
-    fs.writeFile(`${OUTPUT_PATH}/rules.json`, JSON.stringify({ rules }), 'utf8', err => {
-      if(err) {
-        return console.log(chalk.red("Couldn't write file."));
-      }
+    this.stopSpinner();
 
-      this.stopSpinner();
-      console.log(chalk.green(`The JSON was successfully generated and saved at ${OUTPUT_PATH}/rules.json`));
-    })
+    if(program.html) {
+      return console.log(chalk.green(`The HTML and JSON files were successfully generated and saved in ${OUTPUT_PATH}`));
+    }
+
+    return console.log(chalk.green(`The JSON file was successfully generated and saved at ${OUTPUT_PATH}/${JSON_FILENAME}`));
   }
 }
 
 const staticRuleSite = new SiteGenerator();
 
-if(program.html) {
-  staticRuleSite.generateHTMLFile();
-}
-
-if(program.json) {
-  staticRuleSite.generateJSONFile();
-}
+staticRuleSite.generateStaticFiles();
